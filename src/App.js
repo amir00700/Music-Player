8eb1c18dab6de2edc40f287927f2c83cc2340322
amir00700 <43377462+amir00700@users.noreply.github.com>
2021-04-11 12:17:14 -0400

    Library & Library song component added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import React,{useState} from "react";
+import React,{useState,useRef} from "react";
 //import styles
 import   './styles/app.scss';
 //adding components
 import Player from './components/Player';
 import Song from './components/Song';
+import Library from './components/Library'
 
 //import util
 import data from "./util";
+import { library } from "@fortawesome/fontawesome-svg-core";
 
 
 function App() {
   //state
 const[songs, setSongs] = useState(data());
 const[currentSong, setCurrentSong] = useState(songs[0]);
+const [isPlaying, setIsPlaying] = useState(false);
+const audioRef= useRef(null);
+const [songInfo,setSongInfo] = useState({
+  currentTime: 0,
+  duration: 0,
+})
+
+const timeUpdatehandler =(e) =>{
+  const current = e.target.currentTime;
+  const duration= e.target.duration;
+  setSongInfo({...setSongInfo,currentTime:current, duration: duration});
+
+}
 
   return (
     <div className="App">
       <Song currentSong={currentSong}/>
-      <Player currentSong={currentSong}/>
+    <Player 
+    setSongInfo={setSongInfo}
+    songInfo={songInfo}
+    audioRef={audioRef}
+    isPlaying={isPlaying} 
+    setIsPlaying={setIsPlaying}
+     currentSong={currentSong}/>
+     <Library isPlaying={isPlaying} 
+     audioRef={audioRef}
+      songs={songs} 
+      setCurrentSong={setCurrentSong}
+      setSongs={setSongs}
+      />
+     <audio onTimeUpdate={timeUpdatehandler} 
+     onLoadedMetadata={timeUpdatehandler}
+     ref={audioRef}
+     src={currentSong.audio}></audio>
       
     </div>
   );
---
+++ src/components/Librarysong.js
@@ -0,0 +1,53 @@
+import { faCommentsDollar } from '@fortawesome/free-solid-svg-icons';
+import React from 'react'
+
+const Librarysong = ({song, songs, setCurrentSong,id, audioRef,isPlaying,setSongs}) => {
+
+    const songSelectHandler = async ()=>{
+        await setCurrentSong(song);
+        //Add Active State
+          const newSongs= songs.map((song)=>{
+              if(song.id === id){
+                  return {
+                      ...song,
+                      active:true,
+
+                  }}
+                  else
+                  {
+                      return{
+                          ...song,
+                          active:false,
+
+                      }
+                  }
+              }
+          )
+          setSongs(newSongs);
+
+        //check if the song is playing
+        if(isPlaying){
+            const playPromise =audioRef.current.play();
+            if(playPromise !==undefined){
+                playPromise.then((audio) =>{
+                    audioRef.current.play();
+                })
+            }
+        }
+
+
+
+    }
+    return (
+        <div  onClick={songSelectHandler} className={`library-song ${song.active ? 'selected' : ""}`}>
+            <img  alt ={song.name} src={song.cover}></img>
+            <div className='song-description'>
+            <h3>{song.name} </h3>
+            <h4>{song.artist}</h4>
+            </div>
+            
+        </div>
+    )
+}
+
+export default Librarysong
